eb6771c4066877ba11316a0ba37f2482e9b76253
Hsing Fu <69998786+revival0728@users.noreply.github.com>
2024-03-12 16:41:49 +0800

    Update connectProvider.ts

--- frontend/lib/connectProvider.ts
+++ frontend/lib/connectProvider.ts
@@ -4,10 +4,10 @@ export function getProvider() {
   let provider = null
 
   if(typeof window === 'undefined') {
-    provider = ethers.getDefaultProvider()
+    provider = ethers.getDefaultProvider('http://localhost:8545/')
   } else if(window.ethereum === undefined) {
     // MetaMask not installed. Using read-only defaults.
-    provider = ethers.getDefaultProvider();
+    provider = ethers.getDefaultProvider('http://localhost:8545/');
   } else {
     provider = new ethers.BrowserProvider(window.ethereum)
   }
